f0c5c3cb52135aaeb768f660510d24d7ee05eea7
AKRITI-SINGH1 <akritijune09@gmail.com>
2025-08-09 23:53:44 +0530

    error

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
